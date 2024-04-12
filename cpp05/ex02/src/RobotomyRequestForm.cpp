fe38f7d8c8baef0fe3685640eb63412d980085f2
Riku Leskinen <rleskine@c1r6p3.hive.fi>
2024-04-12 17:32:07 +0300

    damnit those trees

---
+++ cpp05/ex02/src/RobotomyRequestForm.cpp
@@ -0,0 +1,24 @@
+#include "RobotomyRequestForm.hpp"
+
+// Constructors
+RobotomyRequestForm::RobotomyRequestForm(std::string target)
+	: AForm("RobotomyRequestForm", 72, 45, target) {}
+
+RobotomyRequestForm::RobotomyRequestForm()
+	: RobotomyRequestForm("undefined target") {}
+
+// Destructor
+RobotomyRequestForm::~RobotomyRequestForm() {}
+
+// Methods
+void RobotomyRequestForm::action() const
+{
+	srand(reinterpret_cast<uintptr_t>(this));
+	const bool success = rand() & 1;
+
+	std::cout << "*drill noises*" << std::endl;
+	if (success)
+		std::cout << "Dr Roboman has successfully robotomized his patient " << this->getTarget() << std::endl;
+	else
+		std::cout << "Robotomy attempt at " << this->getTarget() << " has failed!" << std::endl;
+}
--- cpp05/ex02/src/main.cpp
+++ cpp05/ex02/src/main.cpp
@@ -1,19 +1,29 @@
 #include "Bureaucrat.hpp"
 #include "ShrubberyCreationForm.hpp"
+#include "RobotomyRequestForm.hpp"
 
 int main()
 {
 	Bureaucrat b001("b001", 1);
 	std::cout << b001;
+	Bureaucrat b050("b050", 50);
+	std::cout << b050;
 	Bureaucrat b100("b100", 100);
 	std::cout << b100;
 	Bureaucrat b150("b150", 150);
 	std::cout << b150;
 	ShrubberyCreationForm yard("yard");
 	std::cout << yard;
+	ShrubberyCreationForm defaultshrub;
 	b100.signForm(yard);
 	b001.executeForm(yard);
 	b150.executeForm(yard);
+	b150.signForm(defaultshrub);
+	b001.signForm(defaultshrub);
+	b100.executeForm(defaultshrub);
+	RobotomyRequestForm robotomy;
+	b001.signForm(robotomy);
+	b001.executeForm(robotomy);
 	// try
 	// {
 	// 	std::cout << "Trying to create form with sign grade 0" << std::endl;
