73acb50a406098052a70f79ceecd812d6d97c9b5
Riku Leskinen <rleskine@c1r6p3.hive.fi>
2024-05-11 17:01:50 +0300

    stack has mutated

--- cpp08/ex02/include/MutatedStack.hpp
+++ cpp08/ex02/include/MutatedStack.hpp
@@ -12,9 +12,10 @@ class _LIBCPP_TEMPLATE_VIS MutatedStack : public std::stack<_Tp, _Container> {
 			= typename std::stack<_Tp, _Container>::container_type::iterator;
 		using const_iterator
 			= typename std::stack<_Tp, _Container>::container_type::const_iterator;
-
-//	protected:
-//		container_type	c;
+		using reverse_iterator
+			= typename std::stack<_Tp, _Container>::container_type::reverse_iterator;
+		using const_reverse_iterator
+			= typename std::stack<_Tp, _Container>::container_type::const_reverse_iterator;
 
 	public:
 		MutatedStack() : std::stack<_Tp, _Container>() {}
@@ -23,10 +24,16 @@ class _LIBCPP_TEMPLATE_VIS MutatedStack : public std::stack<_Tp, _Container> {
 		
 		MutatedStack& operator=(const MutatedStack& assign) { this->c = assign.c; return *this; }
 
-		iterator begin() { return this->c.begin(); };
-		const_iterator cbegin() { return this->c.cbegin(); };
-		iterator end() { return this->c.end(); };
-		const_iterator cend() { return this->c.cend(); };
+		iterator begin() { return this->c.begin(); }
+		const_iterator cbegin() const { return this->c.cbegin(); }
+		iterator end() { return this->c.end(); }
+		const_iterator cend() const { return this->c.cend(); }
+		reverse_iterator rbegin() { return this->c.rbegin(); }
+		const_reverse_iterator rcbegin() const { return this->c.rbegin(); }
+		reverse_iterator rend() { return this->c.rend(); }
+		const_reverse_iterator rcend() const { return this->c.rend(); }
+
+		
 };
 
 #include "MutatedStack.tpp"
--- cpp08/ex02/src/main.cpp
+++ cpp08/ex02/src/main.cpp
@@ -1,18 +1,18 @@
 #include "MutatedStack.hpp"
 #include <iostream>
+#include <list>
 
 void printFirstLast(MutatedStack<float> fs) {
 	float f = *(fs.begin());
 	std::cout << "stack first: " << f << std::endl;
 	std::cout << "stack last : " << *(--(fs.end())) << std::endl;
-	//std::cout << "stack last : " << *fs.cend() << std::endl;
 }
 
-template<class C, typename I>
-void printContainer(C container) {
-	I begin = container.cbegin();
-	I end = container.cend();
-	std::cout << "Printing container values: {";
+template<typename C>
+void printContainer(std::string name, const C& container) {
+	auto begin = container.rcbegin();
+	auto end = container.rcend();
+	std::cout << "Printing container [" << name << "] values: {";
 	while (begin != end) {
 		std::cout << *(begin++) << " ";
 	}
@@ -25,15 +25,18 @@ int main(void) {
 	fstack.push(42.21f);
 	fstack.push(21.42f);
 	printFirstLast(fstack);
-	//fstack.begin();
 	std::cout << "fstack size = " << fstack.size() << std::endl;
 	std::cout << fstack.top() << std::endl;
 	fstack.pop();
 	std::cout << fstack.top() << std::endl;
-
 	MutatedStack<float>* ptrstack = new MutatedStack<float>();
-	ptrstack->push(5); ptrstack->push(4); ptrstack->push(3); ptrstack->push(2); ptrstack->push(2);
-	printContainer(ptrstack);
+	ptrstack->push(5); ptrstack->push(4); ptrstack->push(3); ptrstack->push(2); ptrstack->push(1);
+	printContainer("*ptrstack", *ptrstack);
 	MutatedStack<float>copyconst(*ptrstack);
-	printContainer(copyconst);
+	printContainer("copyconst", copyconst);
+	delete ptrstack;
+	printContainer("copyconst after ptrstack delete", copyconst);
+	MutatedStack<float>copyassign;
+	copyassign = copyconst;
+	printContainer("copyassign", copyassign);
 }
